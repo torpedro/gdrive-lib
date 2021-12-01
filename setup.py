ee73dc0bb166bd66fa477fbd8ba4462e58f02eb1
Pedro <pedroflemming@gmail.com>
2021-12-01 21:07:00 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,32 @@
 from setuptools import setup, find_packages
 
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
 setup(
-    name='gdrive_lib',
-    version='1.0',
-    packages=find_packages(include=['gdrive_lib', 'gdrive_lib.*'])
+    name="gdrive_lib",
+    version="0.0.1",
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/torpedro/gdrive-lib",
+    license="MIT",
+    packages=["gdrive_lib"],
+    include_package_data=True,
+    install_requires=[
+        "google-api-python-client",
+        "google-auth-httplib2",
+        "google-auth-oauthlib",
+        "oauth2client",
+        "mypy",
+        "pylint"
+    ],
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    }
 )
